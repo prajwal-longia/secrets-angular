190eb6b22c5ad2d5e44cb991e2378e40ab7a4564
Sanidhya Jaiswal <32493033+sanidhyajaiswal@users.noreply.github.com>
2024-05-30 19:51:06 +0530

    signup working

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique:true},
     username: {type: String, required: true, unique:true},
-    password: {type: Number, required: true}
+    password: {type: String, required: true}
 });
 
 userSchema.plugin(uniqueValidator);
