1c035d377678a6f3faef2bd9ee7d46eb1c2fb2fb
prajwal longia <prajwalsingh65@gmail.com>
2024-05-30 20:53:51 +0530

    auth updated

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,11 +2,28 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { AuthData } from "./auth-data.model";
 import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+    private isAuthenticated = false;
+    private token: string;
+    private tokenTimer: any;
+    private authStatusListener = new Subject<boolean>();
 
-    constructor(public http: HttpClient, public router:Router) { }
+    constructor(public http: HttpClient, public router:Router) {}
+
+    getToken() {
+        return this.token;
+    }
+    
+    getIsAuth() {
+        return this.isAuthenticated;
+    }
+    
+    getAuthStatusListener() {
+        return this.authStatusListener.asObservable();
+    }
 
     createUser(email: string, username: string, password: string) {
         const authData: AuthData = {
@@ -21,5 +38,27 @@ export class AuthService {
                 this.router.navigate(["/login"])
             });
     }
+
+    login(username: string, password: string) {
+        const authData = { username: username, password: password };
+        this.http
+          .post<{ token: string; expiresIn: number, message: string }>(
+            "http://localhost:3000/user/login",
+            authData
+          )
+          .subscribe(response => {
+            const token = response.token;
+            this.token = token;
+            if (token) {
+              const expiresInDuration = response.expiresIn;
+              this.isAuthenticated = true;
+              this.authStatusListener.next(true);
+              const now = new Date();
+              const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+              console.log(expirationDate);
+              this.router.navigate(["profile-view"]);
+            }
+          });
+      }
 
 }
